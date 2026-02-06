c4565b2985087926f0283aaaaac0b32754f5a5c9
Valentin Roussel <valentin.rssl@gmail.com>
2026-02-06 07:35:55 +0100

    add tests with unity

---
+++ src/test/test_hash.c
@@ -0,0 +1,72 @@
+#include "unity.h"
+
+#include "../hash.h"
+#include "unity_internals.h"
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+void setUp(void) {
+    srand(time(0));
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+double std_dev(int data[], int N) {
+    long long sum = 0;
+    for (int i = 0; i < N; i++) {
+        sum += data[i];
+    }
+    double avg = sum / (double)N;
+
+    double result = 0;
+    for (int i = 0; i < N; i++) {
+        result += (data[i] - avg) * (data[i] - avg);
+    }
+    return sqrt(result / N);
+}
+
+void random_var_name(int buf_size, char* out) {
+    static const char valid_chars[] =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz"
+      "1234567890"
+      "_";
+    static const int valid_chars_count = sizeof(valid_chars) - 1;
+
+    int len = (rand() % (buf_size - 1)) + 1;
+    for (int i = 0; i < len; i++) {
+        char c = valid_chars[rand() % valid_chars_count];
+        out[i] = c;
+    }
+
+    out[len] = '\0';
+}
+
+void test_hash_function_distribution(void) {
+    int buckets[1024] = {0};
+    const int bucket_count = sizeof(buckets) / sizeof(buckets[0]);
+    char tmp[128];
+    static const int iterations = 100000;
+    for (int i = 0; i < iterations; i++) {
+        random_var_name(sizeof(tmp), tmp);
+        int h = hash(tmp, bucket_count);
+        buckets[h]++;
+    }
+
+    double expected_avg_bucket_size = (double)iterations / bucket_count;
+    double tolerance = 0.5;
+    for (int j = 0; j < bucket_count; j++) {
+        int bucket_count = buckets[j];
+        TEST_ASSERT_LESS_THAN(expected_avg_bucket_size * (1 + tolerance), bucket_count);
+        TEST_ASSERT_GREATER_THAN(expected_avg_bucket_size * (1 - tolerance), bucket_count);
+    }
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_hash_function_distribution);
+    return UNITY_END();
+}
