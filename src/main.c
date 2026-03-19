16cc3d97d6f3d8cf0478bf16a5bcc6cb4ce640cd
Valentin Roussel <git@vrsl.fr>
2026-03-19 21:42:47 +0100

    if stdin is not seekable, use temp file instead

--- src/main.c
+++ src/main.c
@@ -29,7 +29,24 @@ cleanup:
     return ret;
 }
 
+FILE *input_file() {
+    if (fseek(stdin, 0, SEEK_CUR) == 0) {
+        return stdin;
+    }
+
+    FILE *tmp = tmpfile();
+    char buf[4096];
+    size_t n;
+    while ((n = fread(buf, 1, sizeof(buf), stdin))) {
+        fwrite(buf, 1, n, tmp);
+    }
+    rewind(tmp);
+    return tmp;
+}
+
 int main() {
-    int ret = translate(stdin, stdout);
+    FILE *input = input_file();
+    int ret = translate(input, stdout);
+    fclose(input);
     return ret;
 }
